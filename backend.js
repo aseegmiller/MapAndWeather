f26655a64296519d78e385190f976ffa9dfb0afe
tmccarthy67 <deviowa@deviowas-MacBook-Air.local>
2014-07-02 16:39:40 -0500

    testing

--- backend.js
+++ backend.js
@@ -1,11 +1,8 @@
 
 var wundergroundKey = "6f37a0fd2391cbcb";
 var inputStreet = $("#street");
-console.log(inputStreet);
 var inputState = $("#state").val();
-console.log(inputState);
 var inputCity = $("#city").val();
-console.log(inputCity);
 var inputUrl = "http://api.wunderground.com/api/" + wundergroundKey + "/geolookup/conditions/q/" + inputState + "/" + inputCity + ".json"
 
 
@@ -27,18 +24,12 @@ var streetView = "http://maps.googleapis.com/maps/api/streetview?size=600x400&" 
 
 //get submit and return
 $(document).on("click", "#button", function(){
-				var img = $("<img>");
-				img.attr("src", streetView);
-				$(tag).append(img);
+console.log(inputCity);
+console.log(inputState);
+console.log(inputStreet);
+
+				// var img = $("<img>");
+				// img.attr("src", streetView);
+				// $(tag).append(img);
 
 })
-
-// $(document).on("click", ".team-name", function () {
-// 	$(this).next().toggle();
-// 	var visible = $(this).next().is(":visible");
-// 	if (visible) {
-// 		$(this).parent().removeClass('team-collapsed')
-// 	} else {
-// 		$(this).parent().addClass('team-collapsed')
-// 	}
-// })
