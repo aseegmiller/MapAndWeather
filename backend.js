e7825b1789b82402d5f5834391c8f28471351447
tmccarthy67 <deviowa@deviowas-MacBook-Air.local>
2014-07-02 16:06:09 -0500

    added streetview

--- backend.js
+++ backend.js
@@ -1,3 +1,15 @@
+
+var wundergroundKey = "6f37a0fd2391cbcb";
+var inputStreet = $("street");
+console.log(inputStreet);
+var inputState = $("#state").val();
+console.log(inputState);
+var inputCity = $("#city").val();
+console.log(inputCity);
+var inputUrl = "http://api.wunderground.com/api/" + wundergroundKey + "/geolookup/conditions/q/" + inputState + "/" + inputCity + ".json"
+}
+
+//get weather here
 jQuery(document).ready(function($) {
   $.ajax({
   url : inputUrl,
@@ -10,15 +22,6 @@ jQuery(document).ready(function($) {
   });
 });
 
-
-
-var wundergroundKey = "6f37a0fd2391cbcb";
-var inputStreet = $("street");
-console.log(inputStreet);
-var inputState = $("#state").val();
-console.log(inputState);
-var inputCity = $("#city").val();
-console.log(inputCity);
-var inputUrl = "http://api.wunderground.com/api/" + wundergroundKey + "/geolookup/conditions/q/" + inputState + "/" + inputCity + ".json"
-}
+//get streetview here
+var streetView = "http://maps.googleapis.com/maps/api/streetview?size=600x400&" + inputStreet + "," + inputCity + "," + inputState;
 
