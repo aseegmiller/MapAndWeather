edb20088d44365f03c3d95938ec68148beb0ac03
tmccarthy67 <deviowa@deviowas-MacBook-Air.local>
2014-07-02 16:45:16 -0500

    button fix

--- backend.js
+++ backend.js
@@ -1,12 +1,13 @@
 
 var wundergroundKey = "6f37a0fd2391cbcb";
-var inputStreet = $("#street");
-var inputState = $("#state").val();
-var inputCity = $("#city").val();
+// var inputStreet = $("#street");
+// var inputState = $("#state").val();
+// var inputCity = $("#city").val();
 var inputUrl = "http://api.wunderground.com/api/" + wundergroundKey + "/geolookup/conditions/q/" + inputState + "/" + inputCity + ".json"
 
 
 //get weather here
+var locationWeather = function (inputUrl) {
 jQuery(document).ready(function($) {
   $.ajax({
   url : inputUrl,
@@ -18,12 +19,17 @@ jQuery(document).ready(function($) {
   }
   });
 });
+}
 
 //get streetview here
 var streetView = "http://maps.googleapis.com/maps/api/streetview?size=600x400&" + inputStreet + "," + inputCity + "," + inputState;
 
 //get submit and return
-$(document).on("click", "#button", function(){
+$(document).on("click", "#submit", function(){
+var inputStreet = $("#street");
+var inputState = $("#state").val();
+var inputCity = $("#city").val();
+
 console.log(inputCity);
 console.log(inputState);
 console.log(inputStreet);
