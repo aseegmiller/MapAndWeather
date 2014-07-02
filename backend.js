4f3ecb5f11523e8bd29d4c9333af9f0150773124
tmccarthy67 <deviowa@deviowas-MacBook-Air.local>
2014-07-02 15:41:08 -0500

    added get input functionality

--- backend.js
+++ backend.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
   $.ajax({
-  url : "http://api.wunderground.com/api/6f37a0fd2391cbcb/geolookup/conditions/q/IA/Cedar_Rapids.json",
+  url : inputUrl,
   dataType : "jsonp",
   success : function(parsed_json) {
   var location = parsed_json['location']['city'];
@@ -9,3 +9,16 @@ jQuery(document).ready(function($) {
   }
   });
 });
+
+
+
+var wundergroundKey = "6f37a0fd2391cbcb";
+var inputStreet = $("street");
+console.log(inputStreet);
+var inputState = $("#state").val();
+console.log(inputState);
+var inputCity = $("#city").val();
+console.log(inputCity);
+var inputUrl = "http://api.wunderground.com/api/" + wundergroundKey + "/geolookup/conditions/q/" + inputState + "/" + inputCity + ".json"
+}
+
