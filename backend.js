b057fcadcdcdf4943f1b38fdd7a9765badda9f34
tmccarthy67 <deviowa@deviowas-MacBook-Air.local>
2014-07-02 16:57:44 -0500

    fixed button

--- backend.js
+++ backend.js
@@ -7,7 +7,7 @@ var wundergroundKey = "6f37a0fd2391cbcb";
 
 
 //get weather here
-var locationWeather = function (inputUrl) {
+/*
 jQuery(document).ready(function($) {
   $.ajax({
   url : inputUrl,
@@ -19,23 +19,26 @@ jQuery(document).ready(function($) {
   }
   });
 });
-}
 
+*/
 //get streetview here
 // var streetView = "http://maps.googleapis.com/maps/api/streetview?size=600x400&" + inputStreet + "," + inputCity + "," + inputState;
 
 //get submit and return
-$(document).on("click", "#submit", function(){
-var inputStreet = $("#street").val();
-var inputState = $("#state").val();
-var inputCity = $("#city").val();
+$(document).on("click", "#submit", function(e){
+	e.preventDefault();
 
-console.log(inputCity);
-console.log(inputState);
-console.log(inputStreet);
+	console.log("here")
+	var inputStreet = $("#street").val();
+	var inputState = $("#state").val();
+	var inputCity = $("#city").val();
+
+	console.log(inputCity);
+	console.log(inputState);
+	console.log(inputStreet);
 
 				// var img = $("<img>");
 				// img.attr("src", streetView);
 				// $(tag).append(img);
 
-})
+			})
