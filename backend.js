7fb75d51dbaf41107b0b60e374c9faeb32f56911
tmccarthy67 <deviowa@deviowas-MacBook-Air.local>
2014-07-02 21:50:29 -0500

    functional interface

--- backend.js
+++ backend.js
@@ -1,29 +1,4 @@
 
-var wundergroundKey = "6f37a0fd2391cbcb";
-var inputStreet = $("#street").val();
-var inputState = $("#state").val();
-var inputCity = $("#city").val();
-var inputUrl = "http://api.wunderground.com/api/" + wundergroundKey + "/geolookup/conditions/q/" + inputState + "/" + inputCity + ".json"
-
-
-//get weather here
-/*
-jQuery(document).ready(function($) {
-  $.ajax({
-  url : inputUrl,
-  dataType : "jsonp",
-  success : function(parsed_json) {
-  var location = parsed_json['location']['city'];
-  var temp_f = parsed_json['current_observation']['temp_f'];
-  alert("Current temperature in " + location + " is: " + temp_f);
-  }
-  });
-});
-*/
-
-//get streetview here
-var streetView = "http://maps.googleapis.com/maps/api/streetview?size=600x400&" + inputStreet + "," + inputCity + "," + inputState;
-
 //get submit and return
 $(document).on("click", "#submit", function(e){
 	e.preventDefault();
@@ -32,8 +7,37 @@ $(document).on("click", "#submit", function(e){
 	var inputState = $("#state").val();
 	var inputCity = $("#city").val();
 
-				var img = $("<img>");
-				img.attr("src", streetView);
-				$(streetview).append(img);
+	//get streetview here
+	var streetView = "http://maps.googleapis.com/maps/api/streetview?size=600x400&location=" + inputStreet + "," + inputCity + "," + inputState;
+	var img = $("<img>");
+	img.attr("src", streetView);
+	$(streetview).append(img);
 
-			})
+//get weather here
+
+var wundergroundKey = "6f37a0fd2391cbcb";
+var inputUrl = "http://api.wunderground.com/api/" + wundergroundKey + "/geolookup/conditions/q/" + inputState + "/" + inputCity + ".json";
+jQuery(document).ready(function($) {
+
+	$.ajax({
+		url : inputUrl,
+		dataType : "jsonp",
+		success : function(cityWeather) {
+			var divMod = $("#weather");
+			var theEnd = "</br>";
+			var location = cityWeather['location']['city'];
+			var currentWeather = cityWeather['current_observation']['weather'];
+			var tempFC = cityWeather['current_observation']['temperature_string'];
+			var feelsLike = cityWeather['current_observation']['fellslike_sting'];
+			var windData = cityWeather['current_observation']['wind_string'];
+					divMod.append($("<p>")
+					.append("The current conditions in " + location + ":" + theEnd)
+					.append("Weather: " + currentWeather + theEnd)
+					.append("Temperature: " + tempFC + theEnd)
+					.append("Feels like: " + feelsLike + theEnd)
+					.append('Wind: ' + windData)
+					)
+		}
+	});
+});
+})
